a9eb1268388294dd029e74a4c7043dce6c9c987e
Zeming <chenz16@rose-hulman.edu>
2019-10-06 02:24:28 -0500

    version 1.0

--- test_motor.py
+++ test_motor.py
@@ -2,7 +2,9 @@ from adafruit_motorkit import MotorKit
 
 Motor = MotorKit()
 
-Motor.motor1.throttle = 1.0
-Motor.motor2.throttle = 0.0
-Motor.motor3.throttle = 0.0
+for i in range(100):
+	Motor.motor1.throttle = 0.0
+	Motor.motor2.throttle = 0.0
+	Motor.motor3.throttle = 0.0
+	Motor.motor4.throttle = 0.5
 Motor.motor4.throttle = 0.0
